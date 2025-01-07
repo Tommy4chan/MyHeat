62b871c0c0551a00387136031bfd7398e5969ab8
Tommy4chan <tommy4chan@gmail.com>
2025-01-07 22:39:00 +0200

    added React useWebSocket library

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,33 +1,29 @@
-import { useEffect, useState } from 'react';
-import useWebSocketStore from './store/websocketStore';
+import { useState } from 'react';
 import Navbar from './components/layout/Navbar';
 import { DashboardPage } from './pages/dashboard';
 import { TemperaturesPage } from './pages/temperatures';
 import { RelaysPage } from './pages/relays';
 import { FunctionsPage } from './pages/functions';
 import { SettingsPage } from './pages/settings';
+import { WebSocketProvider } from './store/websocketStore';
 
 
 function App() {
   const [page, setPage] = useState('dashboard');
 
-  const connect = useWebSocketStore(state => state.connect);
-
-  useEffect(() => {
-    connect();
-  }, []);
-
   return (
-    <div className="flex flex-col min-h-screen bg-gray-900 text-gray-200">
-      <Navbar setPage={setPage} />
-      <div className="container mx-auto h-full my-6 pt-[52px] md:pt-0 px-2 md:px-0">
-        {page === 'dashboard' && <DashboardPage />}
-        {page === 'temperatures' && <TemperaturesPage />}
-        {page === 'relays' && <RelaysPage />}
-        {page === 'functions' && <FunctionsPage />}
-        {page === 'settings' && <SettingsPage />}
+    <WebSocketProvider>
+      <div className="flex flex-col min-h-screen bg-gray-900 text-gray-200">
+        <Navbar setPage={setPage} />
+        <div className="container mx-auto h-full my-6 pt-[52px] md:pt-0 px-2 md:px-0">
+          {page === 'dashboard' && <DashboardPage />}
+          {page === 'temperatures' && <TemperaturesPage />}
+          {page === 'relays' && <RelaysPage />}
+          {page === 'functions' && <FunctionsPage />}
+          {page === 'settings' && <SettingsPage />}
+        </div>
       </div>
-    </div>
+    </WebSocketProvider>
   )
 }
 
--- web/src/store/websocketStore.js
+++ web/src/store/websocketStore.js
@@ -1,71 +1,61 @@
-import { create } from 'zustand';
-import { subscribeWithSelector } from 'zustand/middleware'
+import { create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
+import useWebSocket from "react-use-websocket";
 
-const useWebSocketStore = create(subscribeWithSelector((set, get) => ({
-  socket: null,
-  isConnected: false,
-  messages: {},
+const useWebSocketStore = create(
+  subscribeWithSelector((set) => ({
+    messages: {},
+    isConnected: false,
+    sendMessage: () => {},
+  }))
+);
 
-  connect: () => {
-    if (get().socket) return;
+export const WebSocketProvider = ({ children }) => {
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+    "ws://192.168.1.228/ws",
+    {
+      retryOnError: true,
+      shouldReconnect: () => true,
+      reconnectInterval: 1000,
+      reconnectAttempts: 5,
+      share: true,
+      heartbeat: true,
+      onMessage: (event) => {
+        try {
+          const data = JSON.parse(event.data);
+          const { messageType, ...payload } = data;
 
-    const ws = new WebSocket("ws://192.168.1.228/ws");
-
-    ws.onopen = () => {
-      set({ isConnected: true, socket: ws });
-      console.log("WebSocket connection established");
-    };
-
-    ws.onmessage = (event) => {
-      try {
-        const data = JSON.parse(event.data);
-        const { messageType, ...payload } = data;
-
-        if (messageType) {
-          set((state) => ({
-            messages: {
-              ...state.messages,
-              [messageType]: payload,
-            },
-          }));
+          if (messageType && messageType !== "pong") {
+            useWebSocketStore.setState((state) => ({
+              messages: {
+                ...state.messages,
+                [messageType]: payload,
+              },
+            }));
+          }
+        } catch (error) {
+          console.error("WebSocket message parsing error:", error);
         }
-      } catch (error) {
-        console.error("Error parsing WebSocket message:", error);
-      }
-    };
-
-    ws.onerror = (error) => {
-      console.error("WebSocket Error:", error);
-    };
-
-    ws.onclose = () => {
-      set({ isConnected: false, socket: null });
-    };
-
-    set({ socket: ws });
-  },
-
-  sendMessage: (messageType, payload) => {
-    const { socket } = get();
-    if (socket && socket.readyState === WebSocket.OPEN) {
-      socket.send(
-        JSON.stringify({
-          messageType,
-          payload,
-        })
-      );
-    } else {
-      console.error("WebSocket is not connected");
+      },
+      onOpen: () => {
+        useWebSocketStore.setState({
+          isConnected: true,
+          sendMessage: (messageType, payload) => {
+            sendJsonMessage({
+              messageType,
+              payload,
+            });
+          },
+        });
+      },
+      onClose: () => {
+        console.log("WebSocket disconnected");
+        useWebSocketStore.setState({ isConnected: false });
+      },
     }
-  },
+  );
 
-  disconnect: () => {
-    const { socket } = get();
-    if (socket) {
-      socket.close();
-      set({ socket: null, isConnected: false, messages: {} });
-    }
-  },
-})));
+  return children;
+};
 
 export default useWebSocketStore;
